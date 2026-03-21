3b7c831960ea0b03c9abf38d2ba355db54a9a05c
Shivanshmittal023 <shivanshmittal023@gmail.com>
2026-03-21 19:40:03 +0530

    Refactor Write Back module for improved clarity

--- Pipeline_via_terminal/modules/wb.v
+++ Pipeline_via_terminal/modules/wb.v
@@ -108,26 +108,27 @@ always @(posedge clk or negedge reset) begin
     end
     else if (!stall_read_i && mem_write_i) begin
         wb_write_address_o <= write_address_i;
-        case (alu_operation_i)
+        case (alu_operation_i)  
 
 		// TODO-WB: Store Byte (SB)
             SB: begin
                 wb_write_data_o <= {4{alu_operand2_i[7:0]}};
                 case (write_address_i[1:0])
                     2'b00:  wb_write_byte_o <= 4'b0001;
-                    2'b01:  wb_write_byte_o <= 4'b0010; // TODO
-                    2'b10:  wb_write_byte_o <= 4'b0100; // TODO
-                    default:wb_write_byte_o <= 4'b1000; // TODO
+                    2'b01:  wb_write_byte_o <= 4'b0010;
+                    2'b10:  wb_write_byte_o <= 4'b0100;
+                    2'b11: wb_write_byte_o <= 4'b1000;
+                    default:wb_write_byte_o <= 4'b0000;
                 endcase
             end
 		// TODO-WB: Store Halfword (SH)
             SH: begin
                 wb_write_data_o <= {2{alu_operand2_i[15:0]}};
-                wb_write_byte_o <= write_address_i[1] ? 4'b1100 : 4'b0011; // TODO
+                wb_write_byte_o <= write_address_i[1] ? 4'b1100 : 4'b0011;
             end
             SW: begin
                 wb_write_data_o <= alu_operand2_i;
-                wb_write_byte_o <= 4'b1111; // TODO
+                wb_write_byte_o <= 4'b1111;
             end
             default: begin
                 wb_write_data_o <= 32'hx;
@@ -153,9 +154,10 @@ always @* begin
                 2'b00: wb_read_data_o = {{24{dmem_read_data_i[7]}},  dmem_read_data_i[7:0]};
                 2'b01: wb_read_data_o = {{24{dmem_read_data_i[15]}}, dmem_read_data_i[15:8]};
                 2'b10: wb_read_data_o = {{24{dmem_read_data_i[23]}}, dmem_read_data_i[23:16]};
-                2'b11: wb_read_data_o = {{24{dmem_read_data_i[31]}}, dmem_read_data_i[31:24]}; // TODO
+                2'b11: wb_read_data_o = {{24{dmem_read_data_i[31]}}, dmem_read_data_i[31:24]};
             endcase
         end
+
 
         // load halfword
         LH: wb_read_data_o =
